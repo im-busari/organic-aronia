d20a503ca1a6684880012e2b7470e04c2e0f09db
Immanuella Busari <im.busari@gmail.com>
2019-08-28 18:40:19 +0300

    Initial Commit with translation set

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import { withTranslation, Trans } from 'react-i18next';
+
+class App extends Component {
+  render() {
+
+    const { t, i18n } = this.props;
+        
+    return (
+      <div className="App">
       <header className="App-header">
+            <div>
+                <button onClick={() => i18n.changeLanguage('de')}>de</button>
+                <button onClick={() => i18n.changeLanguage('en')}>en</button>
+            </div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1 className="App-title">
+            { this.props.t('welcome.title', { framework: "Organic Aronia, Bulgaria LTD" }) }
+        </h1>
+        
+          <Trans i18nKey='welcome.intro'>
+              To get started, edit <code>src/App.js</code> and save to reload.
+          </Trans>
+        
       </header>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default withTranslation('common')(App); 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {I18nextProvider} from 'react-i18next';
+import i18next from 'i18next';
+
+import common_de from "./translations/de/common.json";
+import common_en from "./translations/en/common.json";
+
+i18next.init({
+    interpolation: { escapeValue: false },  // React already does escaping
+    lng: 'en',                              // language to use
+    resources: {
+        en: {
+            common: common_en               // 'common' is our custom namespace
+        },
+        de: {
+            common: common_de
+        },
+    },
+});
+
+ReactDOM.render(
+    <I18nextProvider i18n={i18next}>
+        <App/>
+    </I18nextProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
